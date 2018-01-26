3dae2dad033c6e20f4621b86c09a8e733ab1df4b
linzs <linjensor@gmail.com>
2018-01-26 09:34:06 +0800

    增强异常处理

--- src/test/java/cn/linzs/app/AppApplicationTests.java
+++ src/test/java/cn/linzs/app/AppApplicationTests.java
@@ -1,16 +1,29 @@
 package cn.linzs.app;
 
+import cn.linzs.app.common.utils.ShiroUtil;
+import cn.linzs.app.service.CategoryService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.SecurityManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = AppApplication.class)
 public class AppApplicationTests {
+
+	@Autowired
+	private CategoryService categoryService;
 
 	@Test
 	public void contextLoads() {
+		UsernamePasswordToken token = new UsernamePasswordToken();
+		token.setUsername("admin");
+		token.setPassword("admin".toCharArray());
+		ShiroUtil.getSubject().login(token);
 	}
 
 }
